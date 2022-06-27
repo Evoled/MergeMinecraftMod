07b2f4909e096963aab2f4b08674595d2a9248e6
BannanaJoe8ME <jamesdirkworkman@gmail.com>
2022-06-27 13:25:55 -0600

    MCreator Merge commit '6b3470eaa8361af82a185a6c48dbba20904c1771' into 2ce84b7c97e7f4b32486172f083276a149db17c8

--- src/main/java/net/mcreator/mcmerge/init/McmergeModItems.java
+++ src/main/java/net/mcreator/mcmerge/init/McmergeModItems.java
@@ -13,7 +13,10 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.mcmerge.item.WrenchItem;
 import net.mcreator.mcmerge.item.SugarcaneseedItem;
+import net.mcreator.mcmerge.item.StrawberryseedsItem;
+import net.mcreator.mcmerge.item.StrawberryItem;
 import net.mcreator.mcmerge.item.SeedgrassItem;
 import net.mcreator.mcmerge.item.PotatospudsItem;
 import net.mcreator.mcmerge.item.PepperseedsItem;
@@ -29,10 +32,13 @@ import net.mcreator.mcmerge.item.CucumberseedsItem;
 import net.mcreator.mcmerge.item.CucumberItem;
 import net.mcreator.mcmerge.item.CornseedItem;
 import net.mcreator.mcmerge.item.CornItem;
+import net.mcreator.mcmerge.item.CherryseedsItem;
+import net.mcreator.mcmerge.item.CherryItem;
 import net.mcreator.mcmerge.item.CauliflowerseedsItem;
 import net.mcreator.mcmerge.item.CauliflowerItem;
 import net.mcreator.mcmerge.item.CarrotseedsItem;
 import net.mcreator.mcmerge.item.AvacodoseedsItem;
+import net.mcreator.mcmerge.item.AvacadoItem;
 import net.mcreator.mcmerge.McmergeMod;
 
 public class McmergeModItems {
@@ -42,7 +48,7 @@ public class McmergeModItems {
 	public static final RegistryObject<Item> CARROTSEEDS = REGISTRY.register("carrotseeds", () -> new CarrotseedsItem());
 	public static final RegistryObject<Item> LETTUCESEEDS = REGISTRY.register("lettuceseeds", () -> new LettuceseedsItem());
 	public static final RegistryObject<Item> CUCUMBERSEEDS = REGISTRY.register("cucumberseeds", () -> new CucumberseedsItem());
-	public static final RegistryObject<Item> CORNSEED = REGISTRY.register("cornseed", () -> new CornseedItem());
+	public static final RegistryObject<Item> CORNSEEDS = REGISTRY.register("cornseeds", () -> new CornseedItem());
 	public static final RegistryObject<Item> CAULIFLOWERSEEDS = REGISTRY.register("cauliflowerseeds", () -> new CauliflowerseedsItem());
 	public static final RegistryObject<Item> AVACODOSEEDS = REGISTRY.register("avacodoseeds", () -> new AvacodoseedsItem());
 	public static final RegistryObject<Item> OLIVESEEDS = REGISTRY.register("oliveseeds", () -> new OliveseedsItem());
@@ -58,6 +64,12 @@ public class McmergeModItems {
 	public static final RegistryObject<Item> GRAPE = REGISTRY.register("grape", () -> new GrapeItem());
 	public static final RegistryObject<Item> GRAPESEED = REGISTRY.register("grapeseed", () -> new GrapeseedItem());
 	public static final RegistryObject<Item> SUGARCANESEED = REGISTRY.register("sugarcaneseed", () -> new SugarcaneseedItem());
+	public static final RegistryObject<Item> AVACADO = REGISTRY.register("avacado", () -> new AvacadoItem());
+	public static final RegistryObject<Item> STRAWBERRY = REGISTRY.register("strawberry", () -> new StrawberryItem());
+	public static final RegistryObject<Item> CHERRY = REGISTRY.register("cherry", () -> new CherryItem());
+	public static final RegistryObject<Item> CHERRYSEEDS = REGISTRY.register("cherryseeds", () -> new CherryseedsItem());
+	public static final RegistryObject<Item> STRAWBERRYSEEDS = REGISTRY.register("strawberryseeds", () -> new StrawberryseedsItem());
+	public static final RegistryObject<Item> WRENCH = REGISTRY.register("wrench", () -> new WrenchItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
